fa8f8824b5b7eaf16cecb7ae46198c07b12bba4f
Weiwen <gwengww@gmail.com>
2019-08-17 22:54:41 -0500

    refactor union find api

--- src/main/java/UnionFind.java
+++ src/main/java/UnionFind.java
@@ -2,21 +2,24 @@
 
 public class UnionFind {
     private int[] index = null;
+    private int counts;
     public UnionFind (int n) {
         index = new int[n];
         for (int i = 0; i<index.length; i++) {
             index[i] = i;
         }
+        counts = n;
     }
     public void union(int x, int y) {
         if (getIndex(x) != getIndex(y)) {
             int x_root = root(x);
             int y_root = root(y);
             index[x_root] = y_root;
+            counts--;
         }
     }
 
-    public int root(int x) {
+    private int root(int x) {
         while (x != getIndex(x)) {
             index[x] = getIndex(getIndex(x));
             x = getIndex(x);
@@ -24,11 +27,15 @@ public class UnionFind {
         return x;
     }
 
-    public boolean isConnected(int x, int y) {
+    public boolean connected(int x, int y) {
         return root(x) == root(y);
     }
 
-    public int getIndex(int x) {
+    private int getIndex(int x) {
         return index[x];
+    }
+
+    public int counts() {
+        return counts;
     }
 }
