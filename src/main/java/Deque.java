2fbb782dceadf82ff7eaf8232f1ef014d1c0def1
Weiwen Gu <gwengww@gmail.com>
2019-08-18 10:28:29 -0500

    Deque (#2)
    * add Deque
    
    * added RandomDeque

---
+++ src/main/java/Deque.java
@@ -0,0 +1,92 @@
+public class Deque {
+    private int size = 0;
+    int[] arr = new int[4];
+    int head = 1;
+    int tail = this.size+this.head;
+
+    public Deque() {}
+
+    public boolean empty() {
+        return this.size == 0;
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public int first() {
+        if (this.empty()) throw new NullPointerException("Deque is empty.");
+        return this.arr[this.head];
+    }
+
+    public int last() {
+        if (this.empty()) throw new NullPointerException("Deque is empty.");
+        return this.arr[this.tail-1];
+    }
+
+    public void addFirst(int x) {
+        this.size++;
+        if (this.empty()) {
+            this.arr[this.head] = x;
+            this.tail++;
+        } else {
+            this.arr[--this.head] = x;
+        }
+        int current_buffer_size = this.arr.length;
+        if (this.head == 0) {
+            // double array and add buffer slots in front
+            int[] new_arr = new int[current_buffer_size*2];
+            for (int i=0; i<this.tail; i++) {
+                new_arr[i+current_buffer_size] = this.arr[i];
+            }
+            this.arr = new_arr;
+            this.head += current_buffer_size;
+            this.tail += current_buffer_size;
+        }
+    }
+
+    public void addLast(int x) {
+        this.size++;
+        this.arr[this.tail++] = x;
+        int current_buffer_size = this.arr.length;
+        if (this.tail == current_buffer_size) {
+            int[] new_arr = new int[current_buffer_size*2];
+            for (int i=this.head; i<current_buffer_size; i++) {
+                new_arr[i] = this.arr[i];
+            }
+            this.arr = new_arr;
+        }
+    }
+
+    public int popFirst() {
+        if (this.empty()) throw new NullPointerException("Deque is empty");
+        int result = this.arr[this.head++];
+        this.size--;
+        int half_buffer_size = this.arr.length/2;
+        if (this.head >= half_buffer_size) {
+            int[] new_arr = new int[this.arr.length - half_buffer_size/2];
+            for (int i=this.head; i<this.tail; i++) {
+                new_arr[i-half_buffer_size/2] = this.arr[i];
+            }
+            this.arr = new_arr;
+            this.head -= half_buffer_size/2;
+            this.tail = this.head+this.size;
+        }
+        return result;
+    }
+
+    public int popLast() {
+        if (this.empty()) throw new NullPointerException(("Deque is empty"));
+        int result = this.arr[--this.tail];
+        this.size--;
+        int half_buffer_size = this.arr.length/2;
+        if (this.arr.length - this.tail > half_buffer_size/2) {
+            int[] new_arr = new int[this.arr.length - half_buffer_size/2];
+            for (int i=this.head; i<this.tail; i++) {
+                new_arr[i] = this.arr[i];
+            }
+            this.arr = new_arr;
+        }
+        return result;
+    }
+}
