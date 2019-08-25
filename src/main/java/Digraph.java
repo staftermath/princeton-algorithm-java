b5ba95644a4125f7b63f883cbf371af8ba431c2e
Weiwen <gwengww@gmail.com>
2019-08-25 17:19:26 -0500

    added Digraph

---
+++ src/main/java/Digraph.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Digraph extends Graph {
+    public Digraph(int capacity) {
+        super(capacity);
+    }
+
+    @Override
+    public void addEdge(int v, int w) {
+        if (!this.E.containsKey(v)) this.E.put(v, new ArrayList<Integer>());
+        this.E.get(v).add(w);
+    }
+
+    public Digraph reverse() {
+        Digraph reversed = new Digraph(this.V());
+        for (int i=0; i<this.V(); i++) {
+            for (Integer v: this.adj(i)) {
+                reversed.addEdge(v, i);
+            }
+        }
+        return reversed;
+    }
+}
---
+++ src/test/java/DepthFirstSearchForDigraphTests.java
@@ -0,0 +1,66 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.logging.Logger;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class DepthFirstSearchForDigraphTests {
+    private Digraph graph;
+    private static final Logger LOGGER = Logger.getLogger("DFS Test For Digraph Log");
+
+    @BeforeEach
+    public void setUp() {
+        LOGGER.info("Setting Up");
+        this.graph = new Digraph(11);
+        int[] edges = {0, 1, 0, 2, 0, 6, 0, 5, 5, 3, 3, 4, 4, 6, 4, 5, 7, 8, 9, 10};
+        for (int i = 1; i < edges.length; i += 2) {
+            this.graph.addEdge(edges[i - 1], edges[i]);
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6})
+    public void testHasPathToReturnTrue(int vertex) {
+        DepthFirstSearch dfs = new DepthFirstSearch(graph, 0);
+        assertTrue(dfs.hasPathTo(vertex));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {7, 8, 9, 10})
+    public void testHasPathToReturnFalse(int vertex) {
+        DepthFirstSearch dfs = new DepthFirstSearch(graph, 0);
+        assertFalse(dfs.hasPathTo(vertex));
+    }
+
+    @Test
+    public void testPathTo() {
+        DepthFirstSearch dfs = new DepthFirstSearch(graph, 0);
+        Iterator<Integer> path = dfs.pathTo(6).iterator();
+        Integer[] expected = {6, 0};
+        Iterator<Integer> iterExpected = Arrays.asList(expected).iterator();
+        while (path.hasNext()) {
+            assertTrue(iterExpected.hasNext());
+            assertEquals(iterExpected.next(), path.next());
+        }
+        assertFalse(iterExpected.hasNext());
+    }
+
+    @Test
+    public void testHasCycle() {
+        DepthFirstSearch dfsHasCycle = new DepthFirstSearch(graph, 0);
+        assertTrue(dfsHasCycle.hasCycle());
+        Graph graphWithNoCycle = new Graph(11);
+        int[] edges = {0, 1, 0, 2, 0, 6, 0, 5, 5, 3, 3, 4, 7, 8, 9, 10};
+        for (int i = 1; i < edges.length; i += 2) {
+            graphWithNoCycle.addEdge(edges[i - 1], edges[i]);
+        }
+        DepthFirstSearch dfsNoCycle = new DepthFirstSearch(graphWithNoCycle, 0);
+        assertFalse(dfsNoCycle.hasCycle());
+    }
+}
