ad53533e2a01d955dfc1ad39b2705b2dbc5f7112
Weiwen <gwengww@gmail.com>
2019-09-04 20:34:37 -0500

    completed seam carving

--- src/main/java/SeamCarver.java
+++ src/main/java/SeamCarver.java
@@ -2,29 +2,39 @@ import org.jetbrains.annotations.NotNull;
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.logging.Logger;
 
 public class SeamCarver {
-    private ArrayList<ArrayList<Color>> picture;
+    private Color[][] picture;
 
+    int[][] energy;
 
     public SeamCarver(Picture picture) {
-        this.picture = new ArrayList<ArrayList<Color>>();
+        this.picture = new Color[picture.height()][picture.width()];
         for (int i=0; i<picture.height();i++) {
-            this.picture.add(new ArrayList<Color>());
-            for (int j=0; j<picture.width();j++) {
-                this.picture.get(i).add(picture.get(j, i));
+            for (int j = 0; j < picture.width(); j++) {
+                this.picture[i][j] = picture.get(j, i);
+            }
+        }
+        this.calculateEnergy();
+    }
+
+    private void calculateEnergy() {
+        this.energy = new int[this.height()][this.width()];
+        for (int i=0; i<this.height();i++) {
+            for (int j=0; j<this.width();j++) {
+                this.energy[i][j] = this.energy(j, i);
             }
         }
     }
-
     public Picture picture() {
-        int height = this.picture.size();
+        int height = this.picture.length;
         if (height == 0) return null;
-        int width = this.picture.get(0).size();
+        int width = this.picture[0].length;
         Picture result = new Picture(width, height);
         for (int i=0; i<height; i++) {
             for (int j=0; j<width; j++) {
-                result.set(j, i, this.picture.get(i).get(j));
+                result.set(j, i, this.picture[i][j]);
             }
         }
         return result;
@@ -32,33 +42,34 @@ public class SeamCarver {
 
     public int width() {
         if (this.height() == 0) return 0;
-        return this.picture.get(0).size();
+        return this.picture[0].length;
     }
 
     public int height() {
-        return this.picture.size();
+        return this.picture.length;
     }
 
     public int[] findHorizontalSeam() throws QueueEmptyException, QueueFullException  {
+        if (this.height() <= 2) return null;
         EdgeWeightedDigraph graph = new EdgeWeightedDigraph(this.height()*this.width()+2);
-        for (int i=0; i<this.width(); i++) {
-            graph.addEdge(new DirectedEdge(0, i, this.energy(i, 0)));
+        for (int i=0; i<this.height(); i++) {
+            graph.addEdge(new DirectedEdge(0, i+1, this.energy[i][0]));
         }
         for (int j=0; j<this.width()-1; j++) {
             for (int i=1; i<this.height()-1; i++) {
                 int index = this.getHorizontalIndex(i, j);
-                graph.addEdge(new DirectedEdge(index, index+this.height(), this.energy(j+1, i)));
-                graph.addEdge(new DirectedEdge(index, index+this.height()-1, this.energy(j+1, i-1)));
-                graph.addEdge(new DirectedEdge(index, index+this.height()+1, this.energy(j+1, i+1)));
+                graph.addEdge(new DirectedEdge(index, index+this.height(), this.energy[i][j+1]));
+                graph.addEdge(new DirectedEdge(index, index+this.height()-1, this.energy[i-1][j+1]));
+                graph.addEdge(new DirectedEdge(index, index+this.height()+1, this.energy[i+1][j+1]));
             }
             // add left boundary path and right boundary path;
             int topIndex = this.getHorizontalIndex(0, j);
-            graph.addEdge(new DirectedEdge(topIndex, topIndex+this.height(), this.energy(j+1, 0)));
-            graph.addEdge(new DirectedEdge(topIndex, topIndex+this.height()+1, this.energy(j+1, 1)));
+            graph.addEdge(new DirectedEdge(topIndex, topIndex+this.height(), this.energy[0][j+1]));
+            graph.addEdge(new DirectedEdge(topIndex, topIndex+this.height()+1, this.energy[1][j+1]));
 
             int buttomIndex = this.getHorizontalIndex(this.height()-1, j);
-            graph.addEdge(new DirectedEdge(buttomIndex, buttomIndex+this.height()-1, this.energy(j+1, this.height()-1)));
-            graph.addEdge(new DirectedEdge(buttomIndex, buttomIndex+this.height()-2, this.energy(j+1, this.height()-2)));
+            graph.addEdge(new DirectedEdge(buttomIndex, buttomIndex+this.height(), this.energy[this.height()-1][j+1]));
+            graph.addEdge(new DirectedEdge(buttomIndex, buttomIndex+this.height()-1, this.energy[this.height()-2][j+1]));
         }
         // add 0 weight to bottom row to dummy node
         for (int j=0; j<this.height(); j++) {
@@ -70,31 +81,35 @@ public class SeamCarver {
         for (DirectedEdge e: sp.pathTo(graph.V()-1)) {
             if (e.to() != graph.V()-1) seamList.add(this.getRowFromHorizontalIndex(e.to()));
         }
-        return seamList.stream()
+        int[] result = seamList.stream()
                 .mapToInt(i -> (i == null ? 0 : i))
                 .toArray();
+        result[0] = result[1];
+        result[result.length-1] = result[result.length-2];
+        return result;
     }
 
     public int[] findVerticalSeam() throws QueueEmptyException, QueueFullException {
+        if (this.width() <= 2) return null;
         EdgeWeightedDigraph graph = new EdgeWeightedDigraph(this.height()*this.width()+2);
-        for (int i=0; i<this.width(); i++) {
-            graph.addEdge(new DirectedEdge(0, i, this.energy(i, 0)));
+        for (int j=0; j<this.width(); j++) {
+            graph.addEdge(new DirectedEdge(0, j+1, this.energy[0][j]));
         }
         for (int i=0; i<this.height()-1; i++) {
             for (int j=1; j<this.width()-1; j++) {
                 int index = this.getVerticalIndex(i, j);
-                graph.addEdge(new DirectedEdge(index, index+this.width(), this.energy(j, i+1)));
-                graph.addEdge(new DirectedEdge(index, index+this.width()-1, this.energy(j-1, i+1)));
-                graph.addEdge(new DirectedEdge(index, index+this.width()+1, this.energy(j+1, i+1)));
+                graph.addEdge(new DirectedEdge(index, index+this.width(), this.energy[i+1][j]));
+                graph.addEdge(new DirectedEdge(index, index+this.width()-1, this.energy[i+1][j-1]));
+                graph.addEdge(new DirectedEdge(index, index+this.width()+1, this.energy[i+1][j+1]));
             }
             // add left boundary path and right boundary path;
             int leftIndex = this.getVerticalIndex(i, 0);
-            graph.addEdge(new DirectedEdge(leftIndex, leftIndex+this.width(), this.energy(0, i+1)));
-            graph.addEdge(new DirectedEdge(leftIndex, leftIndex+this.width()+1, this.energy(1, i+1)));
+            graph.addEdge(new DirectedEdge(leftIndex, leftIndex+this.width(), this.energy[i+1][0]));
+            graph.addEdge(new DirectedEdge(leftIndex, leftIndex+this.width()+1, this.energy[i+1][1]));
 
             int rightIndex = this.getVerticalIndex(i, this.width()-1);
-            graph.addEdge(new DirectedEdge(rightIndex, rightIndex+this.width()-1, this.energy(this.width()-1, i+1)));
-            graph.addEdge(new DirectedEdge(rightIndex, rightIndex+this.width()-2, this.energy(this.width()-2, i+1)));
+            graph.addEdge(new DirectedEdge(rightIndex, rightIndex+this.width(), this.energy[i+1][this.width()-1]));
+            graph.addEdge(new DirectedEdge(rightIndex, rightIndex+this.width()-1, this.energy[i+1][this.width()-2]));
         }
         // add 0 weight to bottom row to dummy node
         for (int i=0; i<this.width(); i++) {
@@ -106,23 +121,36 @@ public class SeamCarver {
         for (DirectedEdge e: sp.pathTo(graph.V()-1)) {
             if (e.to() != graph.V()-1) seamList.add(this.getColFromVerticalIndex(e.to()));
         }
-        return seamList.stream()
+        int[] result = seamList.stream()
                 .mapToInt(i -> (i == null ? 0 : i))
                 .toArray();
+        result[0] = result[1];
+        result[result.length-1] = result[result.length-2];
+        return result;
     }
 
     public void removeHorizontalSeam(int[] seam) {
-        if (seam.length != this.width()) throw new IndexOutOfBoundsException("seam length and current picture width doesn't match.");
-        for (int i=0; i<seam.length; i++) {
-            this.picture.get(seam[i]).remove(i);
-        };
+        Color[][] newPicture = new Color[this.height()-1][this.width()];
+        for (int j=0; j<this.width(); j++) {
+            for (int i=0; i<this.height(); i++) {
+                if (i<seam[j]) newPicture[i][j] = this.picture[i][j];
+                else if (i>seam[j]) newPicture[i-1][j] = this.picture[i][j];
+            }
+        }
+        this.picture = newPicture;
+        this.calculateEnergy();
     }
 
     public void removeVerticalSeam(int [] seam) {
-        if (seam.length != this.height()) throw new IndexOutOfBoundsException("seam length and current picture height doesn't match.");
-        for (int i=0; i<seam.length; i++) {
-            this.picture.get(i).remove(seam[i]);
-        };
+        Color[][] newPicture = new Color[this.height()][this.width()-1];
+        for (int i=0; i<this.height(); i++) {
+            for (int j=0; j<this.width(); j++) {
+                if (j<seam[i]) newPicture[i][j] = this.picture[i][j];
+                else if (j>seam[i]) newPicture[i][j-1] = this.picture[i][j];
+            }
+        }
+        this.picture = newPicture;
+        this.calculateEnergy();
     }
 
     private int dualGradientEnergy(Color color1, Color color2) {
@@ -134,11 +162,11 @@ public class SeamCarver {
     private int energy(int col, int row) {
         int total = 0;
         if (col > 0 && col < this.width()-1 && row > 0 && row < this.height()-1) {
-            total += dualGradientEnergy(this.picture.get(row).get(col-1), this.picture.get(row).get(col+1));
-            total += dualGradientEnergy(this.picture.get(row-1).get(col), this.picture.get(row+1).get(col));
+            total += dualGradientEnergy(this.picture[row][col-1], this.picture[row][col+1]);
+            total += dualGradientEnergy(this.picture[row-1][col], this.picture[row+1][col]);
         } else {
             // boundary must have maximum energy
-            total = ((int) Math.pow(256, 2))*6;
+            total = this.maxEnergy();
         }
         return total;
     }
@@ -166,20 +194,43 @@ public class SeamCarver {
     private int getHorizontalIndex(int row, int col) {
         return col*this.height()+row+1;
     }
-}
 
-class PixelNode implements Comparable<PixelNode> {
-    final int row;
-    final int col;
-    final int energy;
-    PixelNode(int row, int col, int energy) {
-        this.row = row;
-        this.col = col;
-        this.energy = energy;
+    private int maxEnergy() {
+        return ((int) Math.pow(256, 2))*6;
     }
 
-    @Override
-    public int compareTo(@NotNull PixelNode pixelNode) {
-        return Integer.compare(this.energy, pixelNode.energy);
+    public void save(String filename) {
+        Picture newPicture = new Picture(this.width(), this.height());
+        for (int i=0; i<this.height(); i++) {
+            for (int j=0; j<this.width(); j++) {
+                newPicture.set(j, i, this.picture[i][j]);
+            }
+        }
+        newPicture.save(filename);
+    }
+
+    public static void main(String[] args) throws QueueEmptyException, QueueFullException {
+        String input = args[0];
+        int numberOfTrimming = Integer.parseInt(args[1]);
+        boolean verticalTrimming = Boolean.parseBoolean(args[2]);
+        String output = args[3];
+
+        SeamCarver seamCarver = new SeamCarver(new Picture(input));
+        int iter=0;
+        Logger logger = Logger.getLogger("seam carver");
+        while (iter<numberOfTrimming) {
+            if (iter%100 == 0) logger.info(String.format("Iter %d", iter));
+            if (verticalTrimming) {
+                int[] verticalSeam = seamCarver.findVerticalSeam();
+                if (verticalSeam == null) return;
+                seamCarver.removeVerticalSeam(verticalSeam);
+            } else {
+                int[] horizontalSeam = seamCarver.findHorizontalSeam();
+                if (horizontalSeam == null) return;
+                seamCarver.removeHorizontalSeam(horizontalSeam);
+            }
+            iter++;
+        }
+        seamCarver.save(output);
     }
 }
--- src/test/java/SeamCarverTests.java
+++ src/test/java/SeamCarverTests.java
@@ -28,4 +28,29 @@ public class SeamCarverTests {
         int[] expected = {2, 2, 1, 1};
         assertArrayEquals(expected, horizontalSeam);
     }
-}
+
+    @Test
+    public void testRemoveVerticalSeam() {
+        int[] seam = {2, 2, 1, 1, 2, 2};
+        this.seamCarver.removeVerticalSeam(seam);
+        assertEquals(3, this.seamCarver.width());
+        assertEquals(6, this.seamCarver.height());
+    }
+
+    @Test
+    public void testRemoveHorizontalSeam() {
+        int[] seam = {2, 2, 1, 1};
+        Picture trimmedPicture = new Picture(4, 5);
+        for (int j=0; j<4; j++) {
+            for (int i=0; i<6; i++) {
+                if (i<seam[j]) trimmedPicture.set(j, i, this.picture.get(j, i));
+                else if (i>seam[j]) trimmedPicture.set(j, i-1, this.picture.get(j, i));
+            }
+        }
+        SeamCarver newSeamCarver = new SeamCarver(trimmedPicture);
+        this.seamCarver.removeHorizontalSeam(seam);
+        assertEquals(4, this.seamCarver.width());
+        assertEquals(5, this.seamCarver.height());
+        assertArrayEquals(this.seamCarver.energy, newSeamCarver.energy);
+    }
+ }
